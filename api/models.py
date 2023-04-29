edb719db4b787cc9c855da5022738bbbbe1516cd
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-04-29 16:16:51 -0300

    startapp api

---
+++ api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+
+class Services(models.Model):
+    title = models.CharField(
+        verbose_name='Nome do Serviço',
+        max_length=64
+    )
+    smalldesc = models.TextField(
+        verbose_name='Descrição curta',
+    )
+    largedesc = models.TextField(
+        verbose_name='Descrição longa',
+    )
+    icon = models.FileField(
+        verbose_name='Ícone do Serviço',
+        upload_to="services/",
+        validators=[FileExtensionValidator(["svg"])],
+        help_text="O arquivo necessita ser do tipo SVG.",
+        blank=False,
+    )
+
+    def __str__(self):
+        return self.title
