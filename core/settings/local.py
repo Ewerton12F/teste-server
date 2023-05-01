a8f0e7af8632cb4041fb5eeb73c23b7df281ac4c
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-01 05:33:39 -0300

    newtry

--- core/settings/local.py
+++ core/settings/local.py
@@ -5,3 +5,7 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
+
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
