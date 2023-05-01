20a14a03466f6ba10849060b728cc8193bbd20fe
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-01 04:41:34 -0300

    env8x

--- core/settings/production.py
+++ core/settings/production.py
@@ -20,6 +20,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "127.0.0.1",
+            "PORT": "5432",
+        }
+    }
+
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
