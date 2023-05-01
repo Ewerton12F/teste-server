ffe9a9d2eeb03d7ac8809adf0574b6a24a960ecf
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-01 12:07:19 -0300

    full security

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,5 +1,24 @@
 from .base import *
 
+ALLOWED_HOSTS = ["teste-server-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://teste-server-production.up.railway.app"]
+
+# Secure Cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# HSTS
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# SSL
+SECURE_SSL_REDIRECT = True
+
+SECURE_REFERRER_POLICY = "strict-origin"
+
+SECURE_BROWSER_XSS_FILTER = True
+
 ENGINE = env("ENGINE")
 NAME = env("NAME")
 USER = env("USER")
@@ -17,7 +36,3 @@ DATABASES = {
         "PORT": PORT,
     }
 }
-
-SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
