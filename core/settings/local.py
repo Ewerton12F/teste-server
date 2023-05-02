c08b760f43d4dd551fc8bee34a6803697101d286
Ewerton Freitas <62825484+Ewerton12F@users.noreply.github.com>
2023-05-02 03:46:23 -0300

    os.getenv

--- core/settings/local.py
+++ core/settings/local.py
@@ -3,15 +3,15 @@ from .base import *
 ALLOWED_HOSTS = ["teste-server-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = [
     "http://teste-server-production.up.railway.app/",
-    "https://teste-server-production.up.railway.app/",
+    "https://teste-server-production.up.railway.app",
 ]
 
-ENGINE = env("ENGINE")
-NAME = env("NAME")
-USER = env("USER")
-PASSWORD = env("PASSWORD")
-HOST = env("HOST")
-PORT = env("PORT")
+ENGINE = os.getenv("ENGINE")
+NAME = os.getenv("NAME")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
 
 DATABASES = {
     "default": {
