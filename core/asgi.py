d7fc70390a7cd4dbe7a8e4e4697defb401528926
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-03 00:05:18 -0300

    test prod 9x

--- core/asgi.py
+++ core/asgi.py
@@ -11,6 +11,6 @@ from django.core.asgi import get_asgi_application
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_asgi_application()
