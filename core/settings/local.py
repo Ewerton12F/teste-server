c8586395b189fbcd41fcd7d9fd7a7d86f4fd2de0
Ewerton Freitas <62825484+Ewerton12F@users.noreply.github.com>
2023-05-03 20:59:00 -0300

    test prod 7x

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS += ["127.0.0.1"]
+ALLOWED_HOSTS += HOST
 
 INTERNAL_IPS = ALLOWED_HOSTS
 
--- core/settings/production.py
+++ core/settings/production.py
@@ -1,9 +1,6 @@
 from .base import *
 
-HOST = "teste-server-production.up.railway.app"
-
 ALLOWED_HOSTS += [HOST]
-
 CSRF_TRUSTED_ORIGINS += [f"https://{HOST}"]
 
 # SECURE_SSL_REDIRECT = True
