5e044ba305d8a6ad544696e4ea2b8899a26ff209
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-02 22:28:18 -0300

    test prod 2x

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 ALLOWED_HOSTS += ["teste-server-production.up.railway.app"]
 
-CSRF_TRUSTED_ORIGINS = [f"https://{ALLOWED_HOSTS}"]
+CSRF_TRUSTED_ORIGINS += [f"https://{ALLOWED_HOSTS}"]
 
 # SECURE_SSL_REDIRECT = True
 # SESSION_COOKIE_SECURE = True
