d11e7b3cbc03f0fc9e8133f6875c573b36522eb0
Ewerton Freitas <62825484+Ewerton12F@users.noreply.github.com>
2023-05-02 22:38:39 -0300

    it must works
    it must works

--- api/views.py
+++ api/views.py
@@ -1,11 +1,8 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
-
 from .models import Services
 from .serializers import ServicesSerializer
+from rest_framework.generics import ListAPIView
 
 
 class SevicesListView(ListAPIView):
-    permission_classes = (AllowAny,)
     queryset = Services.objects.all()
     serializer_class = ServicesSerializer
