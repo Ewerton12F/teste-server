aa2f0c282a97fcee019454c435eac46509be30d0
Ewerton Freitas <62825484+Ewerton12F@users.noreply.github.com>
2023-05-03 21:05:33 -0300

    test prod 8x

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,6 @@ from django.core.wsgi import get_wsgi_application
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
