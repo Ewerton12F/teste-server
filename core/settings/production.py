5d2a6b24a206d68c3b5ba8b115e1568322ed3b11
Ewerton Freitas <62825484+Ewerton12F@users.noreply.github.com>
2023-05-05 13:41:13 -0300

    CSRF_TRUSTED_ORIGINS

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,10 +1,7 @@
 from .base import *
 
-CORS_ORIGIN_WHITELIST = (f"https://{HOST}", "https://test-website-ecru.vercel.app")
-
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = [f"https://.{HOST}"]
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
