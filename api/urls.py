1d9e7b7a6bad96bf40afdc6b1bd1bd72474579b2
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-05 17:18:08 -0300

    x

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path
-
 from .views import SevicesListView
+from django.urls import path
 
 urlpatterns = [
     path("services-list", SevicesListView.as_view(), name="services-list"),
