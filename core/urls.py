87e6033a4893e00f7579aacc30101590a8fcb178
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-05 20:18:10 -0300

    urlpatterns + static

--- core/urls.py
+++ core/urls.py
@@ -7,9 +7,9 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
