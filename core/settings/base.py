c3b000af501c0c488c026d2b54193d52f4f2ed2f
Ewerton Freitas <62825484+Ewerton12F@users.noreply.github.com>
2023-05-05 21:24:43 -0300

    var/www/{HOST}/static/

--- core/settings/base.py
+++ core/settings/base.py
@@ -87,8 +87,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "../../staticfiles")
+STATIC_URL = "/static/"
+STATIC_ROOT = f"var/www/{HOST}/static/"
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, "static"),
 ]
