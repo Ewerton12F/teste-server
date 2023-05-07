3e761fa769a75f03b6b35f270baed4ce18d74ab3
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-07 09:29:41 -0300

    Class ServiceDetailView(RetrieveAPIView)

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from .models import Services
 from .serializers import ServicesSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
 
 
 class SevicesListView(ListAPIView):
+    queryset = Services.objects.all()
+    serializer_class = ServicesSerializer
+
+
+class ServiceDetailView(RetrieveAPIView):
     queryset = Services.objects.all()
     serializer_class = ServicesSerializer
