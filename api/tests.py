1599c4e08b9de9b041d9a8231f6d2e3fbd937449
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-07 09:30:44 -0300

    class ServiceTests(APITestCase):

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,8 @@
 from .models import Services
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 
 class ModelTesting(TestCase):
@@ -12,3 +15,22 @@ class ModelTesting(TestCase):
         api = self.api
         self.assertTrue(isinstance(api, Services))
         self.assertEqual(str(api), "Test")
+
+
+class ServiceTests(APITestCase):
+    def test_view_service(self):
+        url = reverse("api:services-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #
+    # def create_service(self):
+    #     data = {
+    #         "title": "Test Service Title",
+    #         "smalldesc": "Test Small Description",
+    #         "largedesc": "Test Large Description",
+    #         "icon": "Test_icon.svg",
+    #     }
+    #     url = reverse("api:services-list")
+    #     response = self.client.post(url, data, format="json")
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
