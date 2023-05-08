43e0da8e853c0d0f430237e87314495da202a1d9
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-08 09:27:13 -0300

    core/static/

--- core/settings/base.py
+++ core/settings/base.py
@@ -88,10 +88,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = f"var/www/{HOST}/static/"
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
